a04b1a8bd44d4da126e4bf1907c7515b5e49872c
Екатерина <114433213+EkaterinaGer@users.noreply.github.com>
2025-11-03 20:34:36 +0300

    iouk

--- src/main/java/searchengine/controllers/ApiController.java
+++ src/main/java/searchengine/controllers/ApiController.java
@@ -1,8 +1,6 @@
 package searchengine.controllers;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import searchengine.config.SitesList;
 import searchengine.dto.responses.NotOkResponse;
@@ -14,7 +12,6 @@ import searchengine.services.SearchService;
 import searchengine.services.StatisticsService;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -24,6 +21,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class ApiController {
+
     private final SearchService searchService;
     private final StatisticsService statisticsService;
     private final ApiService apiService;
@@ -32,61 +30,48 @@ public class ApiController {
     private final ExecutorService executor = Executors.newSingleThreadExecutor();
 
     @GetMapping("/statistics")
-    public ResponseEntity<StatisticsResponse> statistics() throws MalformedURLException {
-        return ResponseEntity.ok(statisticsService.getStatistics());
+    public StatisticsResponse statistics() throws IOException {
+        return statisticsService.getStatistics();
     }
 
     @GetMapping("/startIndexing")
-    public ResponseEntity startIndexing() {
+    public Object startIndexing() {
         if (indexingProcessing.get()) {
-            return ResponseEntity.status(HttpStatus.CONFLICT).body(new NotOkResponse("Индексация уже запущена"));
+            return new NotOkResponse("Индексация уже запущена");
         } else {
             executor.submit(() -> {
                 indexingProcessing.set(true);
                 apiService.startIndexing(indexingProcessing);
             });
-            return ResponseEntity.status(HttpStatus.OK).body(new OkResponse());
+            return new OkResponse();
         }
     }
 
     @GetMapping("/stopIndexing")
-    public ResponseEntity stopIndexing() {
+    public Object stopIndexing() {
         if (!indexingProcessing.get()) {
-            return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body(new NotOkResponse("Индексация не запущена"));
+            return new NotOkResponse("Индексация не запущена");
         } else {
             indexingProcessing.set(false);
-            return ResponseEntity.status(HttpStatus.OK).body(new OkResponse());
+            return new OkResponse();
         }
     }
 
     @PostMapping("/indexPage")
-    public ResponseEntity indexPage(@RequestParam String url) throws IOException {
+    public Object indexPage(@RequestParam String url) throws IOException {
         URL refUrl = new URL(url);
         SitePage sitePage = new SitePage();
-        try {
-            sitesList.getSites().stream().filter(site -> refUrl.getHost().equals(site.getUrl().getHost())).findFirst().map(site -> {
-                sitePage.setName(site.getName());
-                sitePage.setUrl(site.getUrl().toString());
-                return sitePage;
-            }).orElseThrow();
-        } catch (RuntimeException ex) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).
-                    body(new NotOkResponse("Данная страница находится за пределами сайтов указанных в конфигурационном файле"));
-        }
+        sitesList.getSites().stream()
+                .filter(site -> refUrl.getHost().equals(site.getUrl().getHost()))
+                .findFirst()
+                .ifPresentOrElse(site -> {
+                    sitePage.setName(site.getName());
+                    sitePage.setUrl(site.getUrl().toString());
+                }, () -> {
+                    throw new IllegalArgumentException(
+                            "Данная страница находится за пределами сайтов указанных в конфигурационном файле");
+                });
         apiService.refreshPage(sitePage, refUrl);
-        return ResponseEntity.status(HttpStatus.OK).body(new OkResponse());
-    }
-
-    @GetMapping("/search")
-    public ResponseEntity<Object> search(
-            @RequestParam(required = false) String query,
-            @RequestParam(required = false) String site,
-            @RequestParam(required = false, defaultValue = "0") Integer offset,
-            @RequestParam(required = false, defaultValue = "20") Integer limit
-    ) throws IOException {
-        if (query == null || query.isBlank()) {
-            return ResponseEntity.badRequest().body(new NotOkResponse("Задан пустой поисковый запрос"));
-        }
-        return searchService.search(query, site, offset, limit);
+        return new OkResponse();
     }
 }
---
+++ src/main/java/searchengine/exception/LemmaException.java
@@ -0,0 +1,6 @@
+package searchengine.exception;
+
+public class LemmaException extends RuntimeException {
+    public LemmaException(String msg){ super(msg);
+    }
+}
